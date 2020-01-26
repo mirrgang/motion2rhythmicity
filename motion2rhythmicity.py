8cd635f1189d66036aefcbc8ada45ba6856ffd5e
mirrgang <melanie.irrgang@citypedia.eu>
2020-01-26 22:04:57 +0100

    compute TLCC for all motion samples and music samples

--- motion2rhythmicity.py
+++ motion2rhythmicity.py
@@ -1,7 +1,10 @@
-from music_features import get_accentuation
+from motion_features import get_jerk
+from motion_features import get_pca
 from motion_features import get_movement_features
+from music_features import get_accentuation
 from scipy import signal
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 def main():
@@ -9,29 +12,62 @@ def main():
     directory_motion = "motion_phone"
     directory_music = "annotations"
     motion = get_movement_features(directory_motion, sample_rate)
+    pre_processed_data = pre_process(motion)
     music = get_accentuation(directory_music, sample_rate)
 
-    music_sample1 = music["annotations/AladdinSane_.csv"]
-    motion_sample_true = motion["motion_phone/AladdinSane_#20_acc.csv"]
-    motion_sample_false = motion["motion_phone/Bad_#20_acc.csv"]
-    cross_correlation_true = signal.correlate(music_sample1, motion_sample_true[:, 0], mode='same')
-    fig, (ax_music_sample1, ax_motion_sample_true, ax_cross_correlation_true) = pyplot.subplots(3, 1, sharex=True)
-    ax_music_sample1.plot(music_sample1)
-    ax_motion_sample_true.plot(motion_sample_true[:, 0])
-    ax_cross_correlation_true.plot(cross_correlation_true)
+    results = compute_TLCC(pre_processed_data, music)
 
-    pyplot.show()
+    #TODO classify and use new music annotations
+
+    #### compute DTW
+    #### TODO compute FFT of accelerometer data and accentuation signal, common frequencies?
 
 
-    cross_correlation_false = signal.correlate(music_sample1, motion_sample_false[:, 0], mode='same')
-    fig, (ax_music_sample1, ax_motion_sample_false, ax_cross_correlation_false) = pyplot.subplots(3, 1, sharex=True)
-    ax_music_sample1.plot(music_sample1)
-    ax_motion_sample_false.plot(motion_sample_false[:, 0])
-    ax_cross_correlation_false.plot(cross_correlation_false)
+    #cross_correlation_false = signal.correlate(music_sample1, motion_sample_false, mode='same')
+    #fig, (ax_music_sample1, ax_motion_sample_false, ax_cross_correlation_false) = pyplot.subplots(3, 1, sharex=True)
+    #ax_music_sample1.plot(music_sample1)
+    #ax_motion_sample_false.plot(motion_sample_false[:, 0])
+    #ax_cross_correlation_false.plot(cross_correlation_false)
 
-    pyplot.show()
+    #pyplot.show()
 
-    cross_correlation_false = signal.correlate(music_sample1, motion_sample_false[:, 0], mode='same') / 128
+    #cross_correlation_false = signal.correlate(music_sample1, motion_sample_false[:, 0], mode='same') / 128
+
+
+def pre_process(data):
+    pca_motion = get_pca(data, 1)
+    return pca_motion
+
+
+def cross_correlation(music_data, motion_data):
+    music_centered = zero_centering(music_data)
+    motion_centered = zero_centering(motion_data)
+    cross_corr = np.correlate(music_centered, motion_centered, mode='full')
+    lag = cross_corr.argmax() #- (len(music_centered) - 1) TODO check this
+    max_corr = cross_corr[lag]
+    print("lag: " + str(lag))
+    print("max corr: " + str(max_corr))
+    return max_corr
+    #plt.plot(cross_corr)
+    #plt.show()
+    #plt.plot(music_centered, 'r')
+    #plt.plot(motion_centered, 'b')
+    #plt.show()
+
+
+def zero_centering(data):
+    return data-np.mean(data)
+
+
+def compute_TLCC(motion_data, music):
+    results = {}
+    for index in motion_data.keys():
+        corrs_per_sample = {}
+        for music_key in music.keys():
+            corrs_per_sample[music_key] = cross_correlation(music[music_key], np.squeeze(motion_data[index]))
+        results[index] = corrs_per_sample
+    return results
+
 
 if __name__ == "__main__":
     main()
--- motion_features.py
+++ motion_features.py
@@ -2,19 +2,22 @@ from numpy import genfromtxt
 from glob import glob
 from numpy import gradient
 from scipy import signal
+from sklearn.decomposition import PCA
 
 
 def get_movement_features(directory, rate):
     motion = get_motion_data(directory)
-    jerk = get_jerk(motion)
-    adjusted_jerk = get_adjusted_motion(jerk, rate)
-    return adjusted_jerk
+    #jerk = get_jerk(motion)
+    adjusted_motion_features = get_adjusted_motion(motion, rate)
+    return adjusted_motion_features
+
 
 def get_adjusted_motion(data, rate):
     adjusted_jerk_data = {}
     for filename, jerk_data in data.items():
         adjusted_jerk_data[filename] = signal.resample(jerk_data, num=rate)
     return adjusted_jerk_data
+
 
 def get_motion_data(directory):
     motion_data = {}
@@ -23,6 +26,14 @@ def get_motion_data(directory):
         data = genfromtxt(file, delimiter=',')
         motion_data[file] = data
     return motion_data
+
+
+def get_pca(data, n_components):
+    pca_data = {}
+    for filename, raw_data in data.items():
+        pca = PCA(n_components=n_components, whiten=True)
+        pca_data[filename] = pca.fit_transform(raw_data)
+    return pca_data
 
 
 def get_jerk(motion_data):
